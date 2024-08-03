20c4f443134b772dc45c8e59217240809d875e3f
sirsathe <101626254+sirsathe@users.noreply.github.com>
2024-08-03 09:19:10 +0530

    new

--- com.FlipkartProject_Qsp/src/main/java/com/ShoppersStack_genericUtility/Base_Test.java
+++ com.FlipkartProject_Qsp/src/main/java/com/ShoppersStack_genericUtility/Base_Test.java
@@ -88,14 +88,14 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 		
 		@BeforeMethod
 		
-		public void beforeMethod() throws IOException{
+		public void beforeMethod() throws IOException, InterruptedException{
 			System.out.println("@BeforeMethod");
 			welcome=new Welcome_Page(driver);
 			WebElement ele = welcome.getloginBtn();
 			
 			wait=new WebDriverWait(driver, Duration.ofSeconds(30));
 			wait.until(ExpectedConditions.elementToBeClickable(ele));
-			
+			Thread.sleep(3000);
 			ele.click();
 			
 			login=new Login_Page(driver);
